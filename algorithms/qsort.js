deb655819766ae7a1943aff1aec36a6e77ad34e3
Bogdan Dolin <d.bogdan@inbox.ru>
2022-08-17 19:50:00 +0700

    Add qsort algorithm

---
+++ algorithms/qsort.js
@@ -0,0 +1,44 @@
+import { test } from "uvu";
+import * as assert from 'uvu/assert'
+
+const qsort = (arr) => {
+	if (arr.length < 2) {
+		return arr;
+	}
+
+	let left = [];
+	let right = [];
+	let first = arr[0];
+
+	for (var i = 1; i < arr.length; i++) {
+		let item = arr[i];
+		if (item <= first) {
+			left.push(item);
+		} else {
+			right.push(item);
+		}
+	}
+
+	return [...qsort(left), first, ...qsort(right)];
+}
+
+const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const reverseArr = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const notSortedArr = [15, 8, 5, 12, 10, 1, 16, 9, 11, 7, 20, 3, 2, 6, 17, 18, 4, 13, 14, 19];
+const equalArr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+const negativeArr = [-1, 0, 5, -10, 20, 13, -7, 3, 2, -3];
+const negativeArrSorted = [-10, -7, -3, -1, 0, 2, 3, 5, 13, 20];
+
+test("qsort", () => {
+	assert.equal(qsort([]), []);
+	assert.equal(qsort([1]), [1]);
+	assert.equal(qsort([1, 2]), [1, 2]);
+	assert.equal(qsort([2, 1]), [1, 2]);
+	assert.equal(qsort([3, 4, 2, 1, 0, 0, 4, 3, 4, 2]), [0, 0, 1, 2, 2, 3, 3, 4, 4, 4]);
+	assert.equal(qsort(sortedArr), sortedArr);
+	assert.equal(qsort(reverseArr), sortedArr);
+	assert.equal(qsort(notSortedArr), sortedArr);
+	assert.equal(qsort(equalArr), equalArr);
+});
+
+test.run();
